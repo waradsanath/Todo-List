d17148ac048cb0254a9dc4c17160820ae15bebf4
waradsanath <warad_sanath_manjunath@s2023.ssts.edu.sg>
2024-06-30 13:21:11 +0800

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Sanath Warad on 30/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
