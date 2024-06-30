318691243c380657e2c26e811adabc41d4ac9b21
waradsanath <warad_sanath_manjunath@s2023.ssts.edu.sg>
2024-06-30 14:37:29 +0800

    Update ContentView.swift

--- Todo List/ContentView.swift
+++ Todo List/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
+struct Item: Identifiable {
+    var id = UUID()
+    var name: String
+    var dueDate: String
+    var dueMonth: String
+    var dueYear: String
+    var subject: String
+    var completed: Bool
+}
+
 struct ContentView: View {
+    @State var homework: [Item] = [
+        Item(name: "New", dueDate: "Select date", dueMonth: "Select month", dueYear: "Select year", subject: "Subject", completed: false)
+    ]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ForEach(homework, id: \.id) { homework in
+                List {
+                    VStack (alignment: .leading) {
+                        Text(homework.name)
+                            .font(.title)
+                            .bold()
+                        Text("Due: \(homework.dueDate) \(homework.dueMonth) \(homework.dueYear)")
+                            .foregroundStyle(Color.gray)
+                            .font(.caption)
+                    }
+                }
+            }
+                .navigationTitle("Homework")
         }
-        .padding()
     }
 }
 
